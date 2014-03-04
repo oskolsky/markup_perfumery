a5972a508f6bf29ff1fd4ce77037066330abd2fe
Maxim Oskolsky <mo@coalla.ru>
2014-03-04 16:16:14 +0400

    Slider

--- assets/javascripts/project.js
+++ assets/javascripts/project.js
@@ -5,8 +5,37 @@
 //****************************************************************************************************
 $(function() {
 
+  //
+  // .. jQuery Tabs init
+  //  
   $('.tabs').tabs();
 
+  //
+  // .. OWL Carousel init
+  //
+  $('.carousel').each(function() {
+
+    var 
+      $carousel = $(this),
+      $prev = $carousel.closest('.section').find('.carousel-nav_i.__prev'),
+      $next = $carousel.closest('.section').find('.carousel-nav_i.__next');
+
+    $carousel.owlCarousel({
+      responsive: false,
+      items : $carousel.data('items')
+    });
+
+    $prev.on('touchstart click', function() {
+      $carousel.trigger('owl.prev');
+      return false;
+    });
+
+    $next.on('touchstart click', function() {
+      $carousel.trigger('owl.next');
+      return false;
+    });
+
+  });
 
 
 
