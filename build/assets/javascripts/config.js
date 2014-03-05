f25e146e2450a6f01951b4368af89dd0a1b98aaa
Maxim Oskolsky <mo@coalla.ru>
2014-03-05 11:14:08 +0400

    Build

---
+++ build/assets/javascripts/config.js
@@ -0,0 +1,16 @@
+var config = {
+  matchMedia: {
+    phone: {
+      minWidth: '320',
+      maxWidth: '640'
+    },
+    tablet: {
+      minWidth: '641',
+      maxWidth: '960'
+    },
+    desktop: {
+      minWidth: '961',
+      maxWidth: '1180'
+    }
+  }
+};
